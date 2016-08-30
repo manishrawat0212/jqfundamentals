c156c0dae9fef302b6165d3a0843f7966b5f3911
manish_rawat <manishrawat0212@gmail.com>
2016-08-30 19:27:57 +0530

    js exercise 5

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -1,26 +1,29 @@
-$(document).ready(function(){
-  var $modules = $("div.module");
-  var tabs = new tabbedNavigation($modules);
-  tabs.init();
-});
-
-function tabbedNavigation($modules){
-  this.$modules = $modules;
+var option = {
+  modules: $("div.module")
 }
 
-tabbedNavigation.prototype.init = function() {
-  this.$modules.hide();
-  var $navigation = $("<ul></ul>").insertBefore(this.$modules.eq(0));
+var TabbedNavigators = function (option){
+  this.modules = option.modules;
+}
 
-  this.$modules.each(function(){
-    var $currentModule = $(this);
-    var $item = $("<li>" + $currentModule.find("h2").text() + "</li>").appendTo($navigation);
-    $item.bind("click", function(){
-      $item.addClass("current").siblings().removeClass("current");
-      $currentModule.show().siblings(".module").hide();
+TabbedNavigators.prototype.addNavigations = function() {
+  this.modules.hide();
+  var navigation = $("<ul></ul>").insertBefore(this.modules.eq(0));
+
+  this.modules.each(function(){
+    var currentModule = $(this);
+    var item = $("<li>" + currentModule.find("h2").text() + "</li>").appendTo(navigation);
+    item.on("click", function(){
+      item.addClass("current").siblings().removeClass("current");
+      currentModule.show().siblings(".module").hide();
     });
   });
 
-  this.$modules.eq(0).show();
-  $navigation.find("li:eq(0)").addClass("current");
+  this.modules.eq(0).show();
+  navigation.find("li:eq(0)").addClass("current");
 };
+
+$(document).ready(function(){
+  var tabs = new TabbedNavigators(option);
+  tabs.addNavigations();
+});

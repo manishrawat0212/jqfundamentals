1b64eb36c9b5c15d31d543298b29d9167fcfb08d
manish_rawat <manishrawat0212@gmail.com>
2016-08-30 14:26:51 +0530

    initial commit

---
+++ exercises/js/sandbox.js
@@ -0,0 +1,15 @@
+// Exercise 1: Selecting
+
+$("div.module");
+
+$("#myListItem");
+$("#myList li").eq(2);
+$("#myList #myListItem");
+
+$("label[for='q']");
+
+$(":hidden").length;
+
+$("img[alt]").length;
+
+$("#fruits tbody tr:even");

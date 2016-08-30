4d8a2c74c48361b6edf4c5745b129e67109e3a67
manish_rawat <manishrawat0212@gmail.com>
2016-08-30 14:39:26 +0530

    js exercise 2

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -13,3 +13,17 @@ $(":hidden").length;
 $("img[alt]").length;
 
 $("#fruits tbody tr:even");
+
+// Exercise 2: Traversing
+
+$("img").each(function(){
+  console.log($(this).attr("alt"));
+});
+
+$("input[name='q']").closest('form').addClass('myClass');
+
+$("#myList li.current").removeClass("current").next().addClass("current");
+
+$("#specials select").closest("form").find("input.input_submit");
+
+$("#slideshow li:first").addClass("current").siblings().addClass("disabled");

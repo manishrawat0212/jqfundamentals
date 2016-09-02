4b06e116981793c047b7e452204b3136c3c2e069
manish_rawat <manishrawat0212@gmail.com>
2016-09-02 19:41:12 +0530

    jq exercise 8

--- exercises/js/slideshow.js
+++ exercises/js/slideshow.js
@@ -1,37 +1,49 @@
 var slideshowOptions = {
-  slideshow : $("#slideshow")
+  slides : $("#slideshow")
 }
 
 var SlideshowCreator = function(slideshowOptions){
-  this.slideshow = slideshowOptions.slideshow;
+  this.slides = slideshowOptions.slides.find("li");
 }
 
-SlideshowCreator.prototype.start   = function() {
-  var slideshowContainer = $("<div></div>");
-  slideshowContainer.addClass('slideshow-div');
-  
-  var thumbnailsContainer = $("<div></div>");
-  thumbnailsContainer.addClass('thumbnails');
-  
-  this.thumbs = this.slideshow.find("img").clone();
-
-  this.slides = this.slideshow.find("li");
-  this.totalSlides = this.slides.length;
-  this.slides.hide();
-
-  this.slideshow.appendTo(slideshowContainer);
-  this.counter = $("<p></p>").appendTo(slideshowContainer);
-  this.thumbs.appendTo(thumbnailsContainer);
-  thumbnailsContainer.appendTo(slideshowContainer);
-  slideshowContainer.prependTo("body");
-
-  this.slides.eq(0).fadeIn(500);
-  this.slideChanger(this.slides.eq(0));
+SlideshowCreator.prototype.start = function() {
+  this.createContainers();
+  this.addThumbnails();
+  this.addStyle();
+  this.beginShow(this.slides.eq(0));
 };
 
-SlideshowCreator.prototype.slideChanger = function(current) {
+SlideshowCreator.prototype.createContainers = function() {
+  this.slideshowContainer = $("<div></div>");
+  this.slideshowContainer.addClass('slideshow-div');
+  this.slideContainer = $("<div></div>");
+  this.thumbnailsContainer = $("<div></div>");
+  this.thumbnailsContainer.addClass('thumbnails');
+};
+
+SlideshowCreator.prototype.addThumbnails = function() {
+  this.thumbs = this.slides.find("img").clone();
+  this.slides.appendTo(this.slideContainer);
+  this.counter = $("<p></p>").appendTo(this.thumbnailsContainer);
+  this.thumbs.appendTo(this.thumbnailsContainer);
+  this.slideContainer.appendTo(this.slideshowContainer);
+  this.thumbnailsContainer.appendTo(this.slideshowContainer);
+  this.slideshowContainer.prependTo("body");
+};
+
+SlideshowCreator.prototype.addStyle = function() {
+  this.slides.addClass("slides");
+  this.thumbnailsContainer.addClass("thumbs");
+  this.totalSlides = this.slides.length;
+  this.slides.hide();
+  this.slides.eq(0).fadeIn(500);
+};
+
+SlideshowCreator.prototype.beginShow = function(current) {
   var _this = this;
-  if($(current).nextAll().length == 0){
+  // if($(current).nextAll().length == 0){
+  // if($(current).is(":nth-last-child(2)")){
+  if($(current).is(":last-child")){  
     next = this.slides.eq(0);
   }
   else{
@@ -39,16 +51,20 @@ SlideshowCreator.prototype.slideChanger = function(current) {
   }
 
   var num = $(current).prevAll().length;
-  this.counter.html("<b>Slide " + (num + 1) + " of " + this.totalSlides + "</b>");
+  var counterText = "<b>Slide " + (num + 1) + " of " + this.totalSlides + "</b>";
+  this.counter.html(counterText);
   this.thumbs.eq(num).addClass("current-thumbnail");
   this.thumbs.eq(num).siblings().removeClass("current-thumbnail");
 
   setTimeout(function() {
-    $(current).fadeOut(400, function() {
-      $(next).fadeIn(400);
-      _this.slideChanger(next);
-    });
+    _this.changeSlide(current, next);
   }, 3000);
+};
+
+SlideshowCreator.prototype.changeSlide = function(current, next) {
+  $(current).fadeOut(400);
+  $(next).fadeIn(400);
+  this.beginShow(next);
 };
 
 $(document).ready(function(){
